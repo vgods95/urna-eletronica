fa4bb3841551a93b1badae244685a154830c9dc0
Vinicius <viniciusgodoyf@gmail.com>
2021-10-06 09:37:58 -0300

    Construção do menu principal, das controllers e das rotas GET

---
+++ ProjetoUrna/Controllers/VotingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoUrna.Controllers
+{
+    public class VotingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult votes()
+        {
+            return View();
+        }
+    }
+}
